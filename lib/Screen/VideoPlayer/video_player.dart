ec8067ccd78b4d4101811251b24287daf45bbb0d
parmeets834 <parmeets834@gmail.com>
2021-04-05 13:26:49 +0530

    Chewie player

--- lib/Screen/VideoPlayer/video_player.dart
+++ lib/Screen/VideoPlayer/video_player.dart
@@ -8,40 +8,181 @@ import 'package:flutter_vlc_player/flutter_vlc_player.dart';
 import 'package:provider/provider.dart';
 import 'file:///D:/Practice%20folder/school_sampleproj/lib/providers/video_player.dart';
 import 'vlc_player_with_controls.dart';
-
+import 'package:chewie/chewie.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 class VideoPlayer extends StatefulWidget {
   static const classname = "/VideoPlayer";
 
   @override
-  _VideoPlayerState createState() => _VideoPlayerState();
+  _ChewieDemoState createState() => _ChewieDemoState();
 }
 
-class _VideoPlayerState extends State<VideoPlayer> {
-  GlobalKey _key = GlobalKey<VlcPlayerWithControlsState>();
-  @override
-  Widget build(BuildContext context) {
-    final provider = Provider.of<VideoPlayerProvider>(context);
-
-
-    return Scaffold(
-      backgroundColor: Colors.black,
-        body: Center(
-      child:  Container(
-        height: MediaQuery.of(context).size.width,
-        width:MediaQuery.of(context).size.width*1.5,        child: VlcPlayerWithControls(key:_key, controller: provider.videoPlayerController),
-      ),
-    ));
-  }
+class _ChewieDemoState extends State<VideoPlayer> {
+  TargetPlatform _platform;
+   VideoPlayerController _videoPlayerController1;
+   VideoPlayerController _videoPlayerController2;
+  ChewieController _chewieController;
 
   @override
   void initState() {
-    final provider = Provider.of<VideoPlayerProvider>(context, listen: false);
-    provider.videoPlayerController = VlcPlayerController.network(
-      'http://techslides.com/demos/sample-videos/small.mp4',
-      hwAcc: HwAcc.FULL,
+    super.initState();
+    initializePlayer();
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController1.dispose();
+    _videoPlayerController2.dispose();
+    _chewieController?.dispose();
+    super.dispose();
+  }
+
+  Future<void> initializePlayer() async {
+    _videoPlayerController1 = VideoPlayerController.network(
+        'https://assets.mixkit.co/videos/preview/mixkit-forest-stream-in-the-sunlight-529-large.mp4');
+    _videoPlayerController2 = VideoPlayerController.network(
+        'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1226-large.mp4');
+    await Future.wait([
+      _videoPlayerController1.initialize(),
+      _videoPlayerController2.initialize()
+    ]);
+    _chewieController = ChewieController(
+      videoPlayerController: _videoPlayerController1,
       autoPlay: true,
-      options: VlcPlayerOptions(),
+      looping: true,
+      // Try playing around with some of these other options:
+
+      // showControls: false,
+      // materialProgressColors: ChewieProgressColors(
+      //   playedColor: Colors.red,
+      //   handleColor: Colors.blue,
+      //   backgroundColor: Colors.grey,
+      //   bufferedColor: Colors.lightGreen,
+      // ),
+      // placeholder: Container(
+      //   color: Colors.grey,
+      // ),
+      // autoInitialize: true,
     );
-    SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "fgfd",
+      theme: ThemeData.light().copyWith(
+        platform: _platform ?? Theme.of(context).platform,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("widget.title"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Center(
+                child: _chewieController != null &&
+                    _chewieController
+                        .videoPlayerController.value.initialized
+                    ? Chewie(
+                  controller: _chewieController,
+                )
+                    : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    CircularProgressIndicator(),
+                    SizedBox(height: 20),
+                    Text('Loading'),
+                  ],
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                _chewieController.enterFullScreen();
+              },
+              child: const Text('Fullscreen'),
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _chewieController.dispose();
+                        _videoPlayerController1.pause();
+                        _videoPlayerController1.seekTo(const Duration());
+                        _chewieController = ChewieController(
+                          videoPlayerController: _videoPlayerController1,
+                          autoPlay: true,
+                          looping: true,
+                        );
+                      });
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16.0),
+                      child: Text("Landscape Video"),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _chewieController.dispose();
+                        _videoPlayerController2.pause();
+                        _videoPlayerController2.seekTo(const Duration());
+                        _chewieController = ChewieController(
+                          videoPlayerController: _videoPlayerController2,
+                          autoPlay: true,
+                          looping: true,
+                        );
+                      });
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16.0),
+                      child: Text("Portrait Video"),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _platform = TargetPlatform.android;
+                      });
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16.0),
+                      child: Text("Android controls"),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _platform = TargetPlatform.iOS;
+                      });
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16.0),
+                      child: Text("iOS controls"),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
